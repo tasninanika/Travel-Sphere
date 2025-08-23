60a126b19535ee16c54cce6856580fdaf3108d9b
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 22:51:32 +0600

    apply style on navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { use, useState } from "react";
+import { Menu, X } from "lucide-react";
+import logo from "../assets/logo.png";
+import { Link, NavLink, useNavigate } from "react-router";
+import Button from "./Button";
+import { FirebaseAuthContext } from "../provider/FirebaseAuthContext";
+
+const Navbar = () => {
+  const { user, logOutUser } = use(FirebaseAuthContext);
+
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const links = (
+    <>
+      <a
+        href="https://the-dragon-news-2443b.web.app/"
+        className="hover:text-yellow-400"
+      >
+        News
+      </a>
+      <NavLink to="/destination" className="hover:text-yellow-400">
+        Destination
+      </NavLink>
+      <NavLink to="/blogs" className="hover:text-yellow-400">
+        Blogs
+      </NavLink>
+      <NavLink to="/contact" className="hover:text-yellow-400">
+        Contact
+      </NavLink>
+      {user && (
+        <NavLink to="/profile" className="hover:text-yellow-400">
+          Profile
+        </NavLink>
+      )}
+    </>
+  );
+
+  const handleLogout = () => {
+    logOutUser()
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <nav className="w-full text-white bg-gray-900">
+      <div className="container mx-auto px-4 md:px-8 lg:px-12 py-3 flex items-center justify-center gap-36 md:gap-12">
+        {/* Logo */}
+        <div>
+          <img
+            onClick={() => navigate("/")}
+            src={logo}
+            alt="Logo"
+            className="w-24"
+          />
+          {/* <span className="text-xl font-bold">Travel <span className="text-yellow-400">Guru</span></span> */}
+        </div>
+
+        {/* Search Box (hidden on mobile) */}
+        <div className="flex-1 mx-6 max-w-md hidden md:block">
+          <input
+            type="text"
+            placeholder="Search your Destination..."
+            className="w-full px-4 py-2 rounded-lg text-white border-white outline-none focus:border-yellow-400 border-2"
+          />
+        </div>
+
+        {/* Desktop Nav Links */}
+        <div className="hidden md:flex items-center gap-6">
+          {links}
+          {user ? (
+            <Button onClick={handleLogout} label="Log Out" />
+          ) : (
+            <Link to="/login">
+              <Button label="login" />
+            </Link>
+          )}
+        </div>
+
+        {/* Hamburger Icon */}
+        <div className="md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isOpen && (
+        <div className="md:hidden bg-black/90 px-4 py-4 space-y-3">
+          <input
+            type="text"
+            placeholder="Search your Destination..."
+            className="w-full px-4 text-white py-2 rounded-lg  outline-none border-2 "
+          />
+          <div className="grid gap-2">{links}</div>
+          {user ? (
+            <Button onClick={handleLogout} label="Log Out" />
+          ) : (
+            <Link to="/login">
+              <Button label="login" />
+            </Link>
+          )}
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
