0a630b3dcdc2bffb3008042338077edfed535ba0
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 23:43:03 +0600

    apply style on spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="w-14 h-14 mx-auto border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+  );
+};
+
+export default Spinner;
