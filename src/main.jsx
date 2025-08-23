201166693363e7442f2caa09934d4394d3b6e611
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 22:59:29 +0600

    setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
