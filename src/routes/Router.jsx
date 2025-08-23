a70c21fe7ad92eb000149ee9c4c36db1de638c7a
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 22:57:11 +0600

    setup router

---
+++ src/routes/Router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../layout/Root";
+import Blogs from "../pages/blog/blogs";
+import Destination from "../pages/destination/destination";
+import Contact from "../pages/contact/Contact";
+import Home from "../components/Home";
+import Login from "../pages/login/Login";
+import Booking from "../pages/booking/Booking";
+import Register from "../pages/register/Register";
+import ErrorPage from "../pages/errorPage/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, Component: Home },
+      { path: "/destination", Component: Destination },
+      { path: "/blogs", Component: Blogs },
+      { path: "/contact", Component: Contact },
+      { path: "/login", Component: Login },
+      { path: "/register", Component: Register },
+      { path: "/booking", Component: Booking },
+    ],
+  },
+]);
+
+export default router;
