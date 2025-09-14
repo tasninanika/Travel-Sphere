59c8b710914773e8d0d3b27df41713c594605170
anikatasnin <jarintasnin09@gmail.com>
2025-09-14 15:16:25 +0600

    apply style on hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import Hero from "./Hero";
 
 const Header = () => {
   return (
-    <div className="background-img">
+    <div
+      className="background-img min-h-screen bg-cover bg-center relative"
+      style={{ backgroundImage: `url(${Hero})` }}
+    >
       <Hero />
     </div>
   );
