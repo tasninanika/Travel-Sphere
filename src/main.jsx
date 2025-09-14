1078706b3c8f472242efabdc37e4a6a02a43aee3
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 00:55:12 +0600

    changed the title

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
-import router from "./routes/router";
+import router from "./routes/Router";
 import ContextProvider from "./provider/ContextProvider";
 
 createRoot(document.getElementById("root")).render(
